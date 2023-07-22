89403c535f8d2742f169b22289cf1423490bb0a7
Sunwoo <sunnyhwang0621@gmail.com>
2023-07-22 14:11:19 +0900

    [feat]

--- WhereIsMyMovie/SearchResultView.swift
+++ WhereIsMyMovie/SearchResultView.swift
@@ -9,18 +9,45 @@ import SwiftUI
 
 struct SearchResultView: View {
     var results: [Movie]
+    var posterHeight: CGFloat = 180
+    var posterWidth: CGFloat = 180 * (27/40)
     var body: some View {
         ScrollView(.vertical) {
-            VStack {
+            VStack(spacing: 20) {
                 ForEach(results) { result in
-                    HStack {
+                    HStack() {
                         AsyncImage(url: result.poster) { image in
+                            image
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: posterWidth, height: posterHeight)
+                        } placeholder: {
+                            ZStack {
+                                Rectangle().fill(Color.gray)
+                                    .frame(width: posterWidth, height: posterHeight)
+                                Image(systemName: "questionmark.square.fill")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 40))
+                            }
                             
                         }
+                        
+                        Spacer()
                     }
+                    
+                    HStack {
+                        Spacer()
+                        Rectangle()
+                            .frame(width: UIScreen.main.bounds.width * 0.9, height: 0.3)
+                            .foregroundColor(.gray)
+                            
+                        Spacer()
+                    }
+                    
                 }
             }
         }
+        .padding(.vertical, 20)
     }
 }
 
